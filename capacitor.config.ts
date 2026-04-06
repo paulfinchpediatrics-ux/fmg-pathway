58eb32b565952edf09e08d9bf9d9b8b6b233bdf2
paulfinchpediatrics-ux <paulfinchpediatrics@gmail.com>
2026-04-05 21:49:26 -0400

    feat: update capacitor config for iOS + Android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,37 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.paulfinchpediatrics.matchamd',
   appName: 'MatchaMD',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      backgroundColor: '#ffffff',
+      androidSplashResourceName: 'splash',
+      androidScaleType: 'CENTER_CROP',
+      showSpinner: false,
+      iosSpinnerStyle: 'small',
+      spinnerColor: '#999999',
+    },
+    StatusBar: {
+      style: 'Default',
+      backgroundColor: '#ffffff',
+    },
+    Keyboard: {
+      resize: 'body',
+      style: 'dark',
+      resizeOnFullScreen: true,
+    },
+  },
+  android: {
+    allowMixedContent: false,
+    captureInput: true,
+    webContentsDebuggingEnabled: false,
+  },
+  ios: {
+    contentInset: 'automatic',
+    scrollEnabled: true,
+  },
 };
 
 export default config;
